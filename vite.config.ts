14ca1f15f9c6dbd5e5884f4a1100aeef4741f8ba
Aleksandr Semenov <j@juvs.dev>
2024-10-13 15:35:19 +0300

    revert adding vue and options page

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,7 @@
 import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
-import vue from '@vitejs/plugin-vue'
 import manifest from './manifest.json'
-import Components from 'unplugin-vue-components/vite';
-import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                app: './src/options/index.html',
-            },
-        },
-    },
-    server: {
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            port: 5173,
-        },
-    },
-    plugins: [
-        vue(),
-        Components({
-            resolvers: [ PrimeVueResolver() ],
-        }),
-        crx({ manifest }),
-    ],
+    plugins: [crx({ manifest })],
   })
